bc4d3df0d567edb8b3388329670a36b0c4657007
MaxxAferist <maxim.lev.8@mail.ru>
2022-05-30 13:39:12 +0300

    Звуки+логика батарейки v.0_1

--- pereim.py
+++ pereim.py
@@ -1,14 +1,21 @@
-path = "data\Transitions_copy"
-import os
-
-dirs = os.listdir(path)
-for dir in dirs:
-    cur_path = path + '/' + dir
-    files = os.listdir(cur_path)
-    photos = []
-    for file in files:
-        if file.endswith(('png', 'jpg')):
-            photos.append(file)
-    for photo in enumerate(photos):
-        if photo[0] % 3 != 0:
-            os.remove(f'{cur_path}/{photo[1]}')
+{
+    '1': {
+        '1.JPEG': 1,
+        '2': {
+            'Elc.JPG': 1,
+            'Elc_Max.JPG': 1
+        }
+    },
+    '2': {
+        '2.JPEG': 1
+    },
+    '3': {
+        '2': {
+            'Elc.JPG': 1,
+            'Max.JPG': 1,
+            'Max_Elc.JPG':
+                1
+        },
+        '2_4': {'Elc_Max.JPG': 1, 'Max_Elc.JPG': 1}, '2_9': {'Elc_Max.JPG': 1, 'Max_Elc.JPG': 1}, '3.JPG': 1, '4': {'Elc.JPG': 1, 'Max.JPG': 1, 'Max_Elc.JPG': 1}, '4_9': {'Elc_Max.JPG': 1, 'Max_Elc.JPG': 1}, '9': {'Elc.JPG': 1, 'Max.JPG': 1, 'Max_Elc.JPG': 1}}, '4': {'3': {'Elc.jpg': 1, 'Max.jpg': 1, 'Max_Elc.jpg': 1}, '4.JPG': 1, '5': {'Elc.jpg': 1, 'Elc_Max.jpg': 1, 'Max.jpg': 1}}, '5': {'4': {'Elc.jpg': 1, 'Max.jpg': 1, 'Max_Elc.jpg': 1}, '5.JPG': 1, '6': {'Elc.jpg': 1, 'Max.jpg': 1,
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                'Max_Elc.jpg': 1}}, '6': {'5': {'Elc.jpg': 1, 'Max.JPG': 1, 'Max_Elc.jpg': 1}, '6.JPG': 1}, '7': {'7.JPG': 1, '8': {'Elc.jpg': 1, 'Max.jpg': 1, 'Max_Elc.jpg': 1}, '9': {'Elc.jpg': 1, 'Max.jpg': 1, 'Max_Elc.jpg': 1}}, '8': {'7': {'Elc.jpg': 1, 'Max.jpg': 1, 'Max_Elc.jpg': 1}, '8.JPG': 1, '9': {'Elc.jpg': 1, 'Max.jpg': 1, 'Max_Elc.jpg': 1}}, '9': {'3': {'Elc.JPG': 1, 'Max.JPG': 1}, '3_7': {'Elc_Max.JPG': 1, 'Max_Elc.JPG': 1}, '3_8': {'Elc_Max.JPG': 1}, '7': {'Elc.JPG': 1, 'Max.JPG': 1, 'Max_Elc.JPG': 1}, '7_8': {'Elc_Max.JPG': 1, 'Max_Elc.JPG': 1}, '8': {'Elc.JPG': 1, 'Max.JPG': 1, 'Max_Elc.JPG': 1}, '9.JPG': 1}
+}
